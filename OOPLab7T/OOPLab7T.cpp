b7462cc28633cc86ba1ec561ff192ecf5da726ee
Shkliar Maksym <shkliar.maksym@chnu.edu.ua>
2023-05-05 23:36:30 +0300

    Шкляр , виконав роботу з Порядіним і Оршовським

--- OOPLab7T/OOPLab7T.cpp
+++ OOPLab7T/OOPLab7T.cpp
@@ -1,23 +1,113 @@
-﻿// OOPLab7T.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+using namespace std;
 
-#include <iostream>
-#include <clocale>
+class Product {
+public:
+    string name;
+    float price;
+    int quantity;
+    Product(string name, float price, int quantity) {
+        this->name = name;
+        this->price = price;
+        this->quantity = quantity;
+    }
+};
 
-int main()
-{
-    setlocale(LC_CTYPE, "ukr");
-    std::cout << "Лабораторна робота №7  \n";
-    std::cout << "Автоматизована система ...  \n";
+class Customer {
+public:
+    string name;
+    float budget;
+    Customer(string name, float budget) {
+        this->name = name;
+        this->budget = budget;
+    }
+
+    bool makeOrder(Product product, int quantity) {
+        if (product.quantity < quantity) {
+            cout << "Недостатньо товару на складі." << endl;
+            return false;
+        }
+        if (product.price * quantity > budget) {
+            cout << "Недостатньо коштів для здійснення покупки." << endl;
+            return false;
+        }
+        product.quantity -= quantity;
+        budget -= product.price * quantity;
+        cout << "Покупка " << product.name << " (" << quantity << " шт.) проведена успішно. Витрачено: " << product.price * quantity << ". Залишок на рахунку: " << budget << "." << endl;
+        return true;
+    }
+};
+
+class Store {
+public:
+    vector<Product> products;
+    void addProduct(string name, float price, int quantity) {
+        Product product(name, price, quantity);
+        products.push_back(product);
+    }
+
+    Product findProduct(string name) {
+        for (int i = 0; i < products.size(); i++) {
+            if (products[i].name == name) {
+                return products[i];
+            }
+        }
+        return Product("", 0, 0);
+    }
+};
+
+class BlackList {
+public:
+    vector<Customer> customers;
+    void addCustomer(Customer customer) {
+        customers.push_back(customer);
+        cout << "Покупець " << customer.name << " доданий до чорного списку." << endl;
+
+        ofstream blacklistFile; // створюємо об'єкт для запису в файл
+        blacklistFile.open("blacklist.txt", ios::app); // відкриваємо файл для додавання даних в кінець
+
+        if (blacklistFile.is_open()) { // перевіряємо, чи відкрито файл
+            blacklistFile << customer.name << endl; // записуємо ім'я клієнта в файл
+            blacklistFile.close(); // закриваємо файл
+        }
+        else {
+            cout << "Помилка відкриття файлу." << endl;
+        }
+    }
+
+    bool checkCustomer(Customer customer) {
+        for (int i = 0; i < customers.size(); i++) {
+            if (customers[i].name == customer.name) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
+
+int main() {
+    Store store;
+    store.addProduct("Телефон", 10000, 10);
+    store.addProduct("Ноутбук", 20000, 5);
+    Customer customer("Олег", 15000);
+
+    cout << "Покупець " << customer.name << ". Бюджет: " << customer.budget << "." << endl;
+
+    Product product = store.findProduct("Телефон");
+    if (product.name == "") {
+        cout << "Вказане не вірна назва товару." << endl;
+        return 0;
+    }
+
+    cout << "Товар: " << product.name << ". Ціна: " << product.price << ". Кількість: " << product.quantity << "." << endl;
+
+    if (!customer.makeOrder(product, 2)) {
+        BlackList blackList;
+        blackList.addCustomer(customer);
+    }
+
+    return 0;
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
